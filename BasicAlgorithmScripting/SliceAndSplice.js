0035ecf04bdbd2d541dbb055a84f21d2d6ee68bd
Jason Serafica - they/them <13142485+seraficaj@users.noreply.github.com>
2021-09-20 20:37:16 -0700

    Create SliceAndSplice.js

---
+++ BasicAlgorithmScripting/SliceAndSplice.js
@@ -0,0 +1,18 @@
+/*
+Slice and Splice
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  let spliced = [...arr2];
+  spliced.splice(n, 0, ...arr1)
+  return spliced;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
